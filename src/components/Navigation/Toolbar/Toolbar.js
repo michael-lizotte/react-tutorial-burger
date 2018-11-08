4a1c6a09ec20f316134a5540e048d14e8834a9a5
michael-lizotte <michael.lizotte-gagnon1@uqac.ca>
2018-11-08 12:40:29 -0500

    Added responsive design to Toolbar and SideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './Toolbar.css';
 
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ...
+        <Logo className="Logo"/>
+        <nav className="DesktopOnly">
+            <NavigationItems/>
         </nav>
     </header>
 );
