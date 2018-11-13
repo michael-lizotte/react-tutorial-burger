e6788f8a9b72c41ab6b87586648786e207f8bc59
michael-lizotte <michael.lizotte-gagnon1@uqac.ca>
2018-11-13 16:42:42 -0500

    Fetched orders in firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import axios from '../../axios-order';
 
 import Order from '../../components/Order/Order';
+import ErrorHandler from '../../wrappers/ErrorHandler/ErrorHandler';
 
 class Orders extends Component {
+    state = {
+        order: [],
+        loading: true
+    }
+
     render() {
         return (
             <div>
@@ -11,6 +18,22 @@ class Orders extends Component {
             </div>
         );
     }
+
+    componentDidMount() {
+        axios.get('/orders.json').then((res) => {
+            const fetchedOrders = [];
+            for (let key in res) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            console.log(fetchedOrders);
+            this.setState({loading:false, orders: fetchedOrders})
+        }).catch((err) => {
+            this.setState({loading:false})
+        })
+    }
 }
 
-export default Orders;
+export default ErrorHandler(Orders, axios);
